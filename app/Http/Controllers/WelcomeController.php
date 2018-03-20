aaf871113052c57c55528e9c920dcaa9f7f359f6
linda <>
2018-03-19 18:22:09 -0700

    set up product-review laravel

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function index(){
+      $dog = 'gunther is cool';
+      $cat = 'meow';
+      return view('welcome', compact('dog', 'cat'));
+    }
+}
